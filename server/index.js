7160dd7de68050a82e57b3dc9d19ebbcf6df5f65
Jobways03 <161472278+Jobways03@users.noreply.github.com>
2025-05-12 14:46:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,46 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-// configuring environmental variables
+const authRoutes = require("./routes/auth");
+const jerseyteamRoutes = require("./routes/team");
+const orderRoutes = require("./routes/order");
+const authMW = require("./middleware/auth");
+const logosRoutes = require("./routes/logos");
+
+const mailerRoutes = require("./src/routes/mailer");
+const imageRoutes = require("./src/routes/image");
+const videoRoutes = require("./src/routes/video");
+const scheduleRoutes = require("./src/routes/schedule");
+const teamRoutes = require("./src/routes/team");
+const playerRoutes = require("./src/routes/player");
+
+const app = express();
+
 dotenv.config();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
+
+// Routes
+app.use("/comment", mailerRoutes);
+app.use("/image", imageRoutes);
+app.use("/video", videoRoutes);
+app.use("/schedule", scheduleRoutes);
+app.use("/team", teamRoutes);
+app.use("/player", playerRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/team/logos", authMW, logosRoutes);
+app.use("/api/team", authMW, jerseyteamRoutes);
+app.use("/api/order", authMW, orderRoutes);
+
+// Mongo + Listen
 const port = process.env.PORT || 8000;
 const db_url = process.env.DATABASE_URL;
-const con = process.env.NODE_ENV;
 
 mongoose
   .connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
